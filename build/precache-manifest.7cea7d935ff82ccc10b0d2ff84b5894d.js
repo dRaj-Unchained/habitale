5a09f903633d55f131682bdc4179b1ba2e0c90e2
Yadav, Raj Kumar <rajkumaryadav@spglobal.com>
2025-04-30 17:26:55 +0530

    add

---
+++ build/precache-manifest.7cea7d935ff82ccc10b0d2ff84b5894d.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "779e1d155abb4d79c7b9d11aaf49cbdd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9be1a724621f5e6646d7",
+    "url": "/static/css/2.c3eece98.chunk.css"
+  },
+  {
+    "revision": "78bd7935ac62cf9dd8e9",
+    "url": "/static/css/main.729bcbad.chunk.css"
+  },
+  {
+    "revision": "9be1a724621f5e6646d7",
+    "url": "/static/js/2.f2fb06e8.chunk.js"
+  },
+  {
+    "revision": "fb38d53809f7fd861674",
+    "url": "/static/js/3.0190765b.chunk.js"
+  },
+  {
+    "revision": "78bd7935ac62cf9dd8e9",
+    "url": "/static/js/main.f029960f.chunk.js"
+  },
+  {
+    "revision": "24c30e9bdfb0436d29e8",
+    "url": "/static/js/runtime~main.25ef6dca.js"
+  },
+  {
+    "revision": "ec0c827e6b5c6bdbddab4194e3268e1a",
+    "url": "/static/media/about.ec0c827e.png"
+  },
+  {
+    "revision": "e8bd59a29882f02883b5ba9cd50da6f6",
+    "url": "/static/media/capacity.e8bd59a2.svg"
+  },
+  {
+    "revision": "25919c237e12069b5c2946a81e26ceb0",
+    "url": "/static/media/hero1.25919c23.png"
+  },
+  {
+    "revision": "72707fd74b617c12c73a9c2dac4fdcb3",
+    "url": "/static/media/hero2.72707fd7.png"
+  },
+  {
+    "revision": "d3f45ee99549d6b7aac0958582f0d38d",
+    "url": "/static/media/hero3.d3f45ee9.png"
+  },
+  {
+    "revision": "3b191cf95115b8b253d76cc56787b755",
+    "url": "/static/media/icon02.3b191cf9.png"
+  },
+  {
+    "revision": "3b42158ef579bfbe9a733fa5fcf82478",
+    "url": "/static/media/icon03.3b42158e.png"
+  },
+  {
+    "revision": "9c1d31d47f9e2ded901ef4d0a1f3b228",
+    "url": "/static/media/icon04.9c1d31d4.png"
+  },
+  {
+    "revision": "a497ba4d2533fbf1c8776bfce0844a42",
+    "url": "/static/media/icon06.a497ba4d.png"
+  },
+  {
+    "revision": "1c216b3b9756f1d652068e32f8e16183",
+    "url": "/static/media/icon07.1c216b3b.png"
+  },
+  {
+    "revision": "cd99387ad5d5a402829416b786836ac9",
+    "url": "/static/media/icon08.cd99387a.png"
+  },
+  {
+    "revision": "438bea0809a0b23be60449b7f560662f",
+    "url": "/static/media/icon5.438bea08.png"
+  },
+  {
+    "revision": "6c00629b85b430ff670de2d4964a294d",
+    "url": "/static/media/logo.6c00629b.png"
+  },
+  {
+    "revision": "264a32e97f88dbe8721e65e84fd2a194",
+    "url": "/static/media/mission.264a32e9.svg"
+  },
+  {
+    "revision": "dde27574897c45a3671f7fdd4104638b",
+    "url": "/static/media/vision.dde27574.svg"
+  }
+]);
